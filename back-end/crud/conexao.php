aa74ba9b42d6e5e62b7d8d07a67378e9ab55ceb3
Lucas07155 <lucaspraxedes74@gmail.com>
2024-12-11 23:11:04 -0300

    commit

--- back-end/crud/conexao.php
+++ back-end/crud/conexao.php
@@ -1,6 +1,6 @@
 <?php
-$pdo = new PDO("mysql:dbname=academia;host=localhost;port=3306", "root", "");
+$pdo = new PDO("mysql:dbname=academia;host=localhost;port=3307", "root", "lucas123");
 if (!$pdo) {
     echo "Acesso negado!";
 } 
-?>
+?>gagsjhagsjhags
